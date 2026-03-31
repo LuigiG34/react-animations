b966253592a7fa1d70bad202605223cf6d7afdb7
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-31 15:26:20 +0200

    Animating staggered lists

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -70,7 +70,7 @@ export default function NewChallenge({ onDone }) {
                       hidden: {opacity: 0, scale: 0.5},
                       visible: {opacity: 1, scale: 1},
                     }}
-                    exit="visible"
+                    exit={false}
                     transition={{type: 'spring'}}
                     key={image.alt}
                     onClick={() => handleSelectImage(image)}
