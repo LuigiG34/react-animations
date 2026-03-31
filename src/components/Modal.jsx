50a62a86108f22eb95c479dbf870b9725aec823d
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-31 15:13:02 +0200

    Nested animations & variants

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,12 +7,12 @@ export default function Modal({ title, children, onClose }) {
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
           variants={{
-              hidden: {opacity: 0, y: 30},
+              hiddenModal: {opacity: 0, y: 30},
               visible: {opacity: 1, y: 0},
           }}
-          initial="hidden"
+          initial="hiddenModal"
           animate="visible"
-          exit="hidden"
+          exit="hiddenModal"
           open className="modal">
         <h2>{title}</h2>
         {children}
